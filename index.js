5bd989db2b148f6d8cec9bbd88770dc6cbb33cea
Frédéric Pluquet <fredericpluquet@gmail.com>
2022-11-25 11:05:51 +0100

    Ajout d'un commentaire

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 
 const app = express()
 
-app.use(express.static("public")) // commentaire
+app.use(express.static("public")) // commentaire dsfjshdfkjsdhfkj
 
 // jhsjdh
 
