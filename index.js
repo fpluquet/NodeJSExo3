0faf68424c166fa66c855421dd1dbc48245bb7d5
Frédéric Pluquet <fredericpluquet@gmail.com>
2022-11-25 10:49:24 +0100

    Initialisation du git

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+  res.send("<h1>Bonjour :D</h1>" + Date())
+})
+
+
+app.listen(3000, () => console.log("Le serveur tourne !"))
